d797808c35bd7e2d90cc3fc4207f4baa1d05d17d
Bernhard Graf <augensalat@gmail.com>
2010-09-05 19:29:46 +0200

    Fixing RSS feed formatting.

--- lib/RavLog/Controller/Feed.pm
+++ lib/RavLog/Controller/Feed.pm
@@ -2,82 +2,79 @@ package RavLog::Controller::Feed;
 
 use strict;
 use warnings;
-use base 'Catalyst::Controller';
+use utf8;
+
+use parent 'Catalyst::Controller';
+
 use XML::Feed;
-use Text::Textile qw(textile);
 
-sub comments : Local
-{
-   my ( $self, $c, $subject ) = @_;
+sub comments : Local {
+    my ($self, $c, $subject) = @_;
+    my $feed = XML::Feed->new('RSS');
+    my @comments;
 
-   my $feed = XML::Feed->new('RSS');
-   $feed->title( $c->config->{name} . ' RSS' );
-   $feed->link( $c->uri_for('/') );
-   $feed->description( $c->config->{name} . ' RSS Feed' );
+    $feed->title($c->config->{name} . ' RSS');
+    $feed->link($c->uri_for('/'));
+    $feed->description($c->config->{name} . ' RSS Feed');
 
-   my @comments;
-   if ( !defined $subject )
-   {
-      @comments = $c->model('DB::Comment')->all();
-   }
-   else
-   {
-      @comments =
-         $c->model('DB::Article')
-         ->search( { subject => { like => $c->ravlog_url_to_query($subject) } } )->first()
-         ->comments();
-   }
+    if (!defined $subject) {
+	@comments = $c->model('DB::Comment')->all();
+    }
+    else {
+	@comments =
+	    $c->model('DB::Article')
+	    ->search({subject => {like => $c->ravlog_url_to_query($subject)}})->first
+	    ->comments;
+    }
 
-   for ( my $i = 0; $i < scalar(@comments); $i++ )
-   {
-      my $feed_entry = XML::Feed::Entry->new('RSS');
-      $feed_entry->title(
-         $comments[$i]->name() . "'s comment " . $comments[$i]->created_at->ymd() );
-      my $url = $c->ravlog_txt_to_url( $comments[$i]->article()->subject() );
-      $feed_entry->link( $c->uri_for( '/view/' . $url . '#comments' ) );
-      $feed_entry->summary( textile( $comments[$i]->comment() ) );
-      $feed_entry->issued( $comments[$i]->created_at() );
-      $feed->add_entry($feed_entry);
-   }
+    for my $com (@comments) {
+	my $feed_entry = XML::Feed::Entry->new('RSS');
 
-   $c->res->content_type('application/rss+xml');
-   $c->res->body( $feed->as_xml );
+	$feed_entry->title($com->name . "'s comment " . $com->created_at->ymd);
+	$feed_entry->link($c->uri_for(
+	    '/view/' . $c->ravlog_txt_to_url($com->article->subject) . '#comments'
+	));
+	$feed_entry->summary($com->formatted_body);
+	$feed_entry->issued($com->created_at);
+	$feed->add_entry($feed_entry);
+    }
+
+    $c->response->content_type('application/rss+xml');
+    $c->response->body($feed->as_xml);
 }
 
-sub articles : Local
-{
-   my ( $self, $c, $tag ) = @_;
+sub articles : Local {
+    my ($self, $c, $tag) = @_;
+    my $feed = XML::Feed->new('RSS');
+    my @articles;
 
-   my $feed = XML::Feed->new('RSS');
-   $feed->title( $c->config->{name} . ' RSS' );
-   $feed->link( $c->uri_for('/') );
-   $feed->description( $c->config->{name} . ' RSS Feed' );
+    $feed->title($c->config->{name} . ' RSS');
+    $feed->link($c->uri_for('/'));
+    $feed->description($c->config->{name} . ' RSS Feed');
 
-   my @articles;
-   if ( !defined $tag )
-   {
-      @articles = $c->model('DB::Article')->get_latest_articles();
-   }
-   else
-   {
-      @articles =
-         $c->model('DB::Tag')
-         ->search( { name => { like => $c->ravlog_url_to_query($tag) } } )->first()->articles();
-   }
+    if (!defined $tag) {
+	@articles = $c->model('DB::Article')->get_latest_articles;
+    }
+    else {
+	@articles =
+	    $c->model('DB::Tag')
+	    ->search({name => {like => $c->ravlog_url_to_query($tag)}})
+	    ->first
+	    ->articles;
+    }
 
-   for ( my $i = 0; $i < scalar(@articles); $i++ )
-   {
-      my $feed_entry = XML::Feed::Entry->new('RSS');
-      $feed_entry->title( $articles[$i]->subject() );
-      my $url = $c->ravlog_txt_to_url( $articles[$i]->subject() );
-      $feed_entry->link( $c->uri_for( '/view', $url ) );
-      $feed_entry->summary( textile( $articles[$i]->body() ) );
-      $feed_entry->issued( $articles[$i]->created_at() );
-      $feed->add_entry($feed_entry);
-   }
+    for my $a (@articles) {
+	my $feed_entry = XML::Feed::Entry->new('RSS');
 
-   $c->res->content_type('application/rss+xml');
-   $c->res->body( $feed->as_xml );
+	$feed_entry->title($a->subject);
+	$feed_entry->link($c->uri_for('/view', $c->ravlog_txt_to_url($a->subject)));
+	$feed_entry->summary($a->formatted_body);
+	$feed_entry->issued($a->created_at);
+	$feed->add_entry($feed_entry);
+    }
+
+    $c->response->content_type('application/rss+xml');
+    $c->response->body($feed->as_xml);
 }
 
 1;
